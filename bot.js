7a3827c3cd294f575224a5893b37e16d2aa069f4
Diogo <1170979@isep.ipp.pt>
2020-10-15 14:31:59 +0100

    initial

---
+++ bot.js
@@ -0,0 +1,66 @@
+const { config } = require("dotenv");
+const fs = require("fs")
+const ytdl = require("ytdl-core")
+
+const { Client, RichEmbed } = new require("discord.js");
+
+const client = new Client({
+    disableEveryone: true
+})
+
+config({
+    path: __dirname + "/.env"
+})
+
+client.on("ready", () => {
+    console.log("I'm online!")
+
+    client.user.setPresence({
+        status: "online",
+        game: "Tás a olhar para onde seu burro?",
+        type: "WATCHING"
+    })
+    aniversarios();
+    setInterval(aniversarios, 3600000);
+
+})
+
+function dc() {
+    client.voice.connections.first().disconnect();
+}
+
+function aniversarios() {
+    var date2 = new Date();
+    var ho=date2.getUTCHours()+1;
+    console.log(date2.getUTCDate()+"-Hora:"+ho);
+    fs.readFile('./aniversarios.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        var date = new Date();
+        if (ho == 24) {
+            console.log("Meia Noite");
+            date.setDate(new Date().getDate()+1);   
+            var dia = date.getUTCDate();
+            var mes = date.getUTCMonth() + 1;
+            var data2 = JSON.parse(data);
+            var data3 = data2[mes];
+            var data4 = data3[dia];
+            if (data4 != undefined) {
+                var id = data4["DiscordID"];
+                if (id != undefined) {
+                    console.log("Alguem faz anos");
+                    client.channels.cache.get("693560308236550166").send("Parabéns burro <@" + id + ">.");
+                    let canalAniversario = client.guilds.cache.first().members.resolve(id).voice.channelID;
+                    let a = client.channels.cache.get(canalAniversario);
+                    if (a != undefined) {
+                        console.log("Quem faz anos ta ON");
+                        a.join();
+                        client.voice.connections.first().play(ytdl("https://www.youtube.com/watch?v=scboWq7ZQGs", { filter: "audioonly" }));
+                        setTimeout(dc, 65000);
+                    }
+                }
+            }
+        }
+    });
+}
+
+client.login(process.env.BOT_TOKEN);
